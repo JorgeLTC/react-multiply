56d5a53e880b727a65b0630cc4a463c554f6e02c
JorgeLTC <myciudadzacatecas@gmail.com>
2026-05-17 22:49:54 -0600

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+        <div className='min-h-screen bg-zinc-950 text-white flex items-center justify-center'>
+          <h1 className='text-5xl font-bold text-yellow-400'>
+           Tailwind works.   
+          </h1>
+        </div>
+  )
+}
+
